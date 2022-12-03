847a71d198c8833f3992cc4ea03f5a8a98db55aa
hamza2001-max <ellawliet1007@gmail.com>
2022-12-03 22:31:48 +0500

    second commit

---
+++ word_counter/src/index.ts
@@ -0,0 +1,30 @@
+const inquirer = require("inquirer");
+
+const question = [
+  {
+    type: "input",
+    name: "paragraph",
+    message: "Enter the paragraph",
+  },
+];
+
+interface answerInterface {
+  paragraph: string;
+}
+inquirer.prompt(question).then((answers: answerInterface) => {
+  let paragraph = answers.paragraph;
+  let words = 1;
+  let letterCounter = 0;
+  console.log(paragraph);
+  for (let i = 0; i < paragraph.length; i++) {
+    let currentLetter = paragraph[i];
+    if (currentLetter !== " ") {
+        letterCounter++;
+    }
+    if (currentLetter == " ") {
+      words++;
+    }
+  }
+  console.log("The total words are:" + words);
+  console.log("The total characters are " + letterCounter);
+});
